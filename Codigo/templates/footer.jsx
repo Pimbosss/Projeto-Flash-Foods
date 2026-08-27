7547b936ff478a9a75bdc8a35447017eb8e0c48d
Pimbos <pimbos21@gmail.com>
2026-08-27 10:51:41 -0300

    confia

---
+++ Codigo/templates/footer.jsx
@@ -0,0 +1,34 @@
+import "/templates/footer.css"
+
+
+export default function Footer(){
+    return(
+        <footer>
+            <div id="titulo2">
+                <h1>Flash Foods</h1>
+                <p>Delivery que valoriza quem entrega e garante comida rápida para você.</p>
+            </div>
+            <div id="voce" >
+                <h4>Para Você</h4>
+                <a href="#">Fazer Pedido</a>
+                <a href="#">Rastrear Entrega</a>
+                <a href="#">Cupons</a>
+            </div>
+            <div id="parceiros" >
+                <h4>Para parceiros</h4>
+                <a href="#">Seja um Entregador</a>
+                <a href="#">Área do Restaurante</a>
+                <a href="#">Central de Ajuda</a>
+            </div>
+            <div id="empresa" >
+                <h4>Empresas</h4>
+                <a href="#">Sobre Nós</a>
+                <a href="#">Trabalhe Conosco</a>
+                <a href="#">Contato</a>
+            </div>
+            <div id="direitos" >
+                <p>© 2026 Flash Foods. Todos os direitos reservados. Delivery com condições justas para entregadores.</p>
+            </div>
+        </footer>
+    )
+} 
